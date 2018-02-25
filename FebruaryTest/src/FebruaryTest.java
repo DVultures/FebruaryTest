64ed593be6b26440954872edb48dd7ab903133c9
Vultures <something@someth.ing>
2018-02-25 01:30:06 +0100

    Just saying some words
    Improved the output to make more sense
    
    Signed-off-by: Vultures <something@someth.ing>

--- FebruaryTest/src/FebruaryTest.java
+++ FebruaryTest/src/FebruaryTest.java
@@ -7,6 +7,6 @@ public class FebruaryTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Gimme February, gimme Test!");
+		System.out.println("Gimme February, gimme Test, gimme FebruaryTest!");
 	}
 }
