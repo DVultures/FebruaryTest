3f236638c1e61ea25d1e52b5075b49ba79172d68
Vultures <something@someth.ing>
2018-02-25 01:27:02 +0100

    Just saying some words
    Signed-off-by: Vultures <something@someth.ing>

---
+++ FebruaryTest/src/FebruaryTest.java
@@ -0,0 +1,12 @@
+/**
+ * @author Vultures
+ */
+public class FebruaryTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Gimme February, gimme Test!");
+	}
+}
